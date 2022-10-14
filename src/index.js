aa2e42073d66ac9ab56519ec4008960817f5bc93
Logesh <logu.siva68@gmail.com>
2022-10-15 01:22:07 +0530

    angular deploy

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 3001
+const path = require('path')
 
 app.use(cors())
 
 app.use(express.json({ limit: 500 }))
 app.use(express.urlencoded({ extended: true }))
-app.use('/',(req,res)=>{
-  res.status(200).json({"status":"health", data:"up"})
-})
+app.use('/c', express.static(path.join(__dirname, 'public/film-ng')))
 app.use('/api', require('./routes'))
 
 app.use('*', (req, res) => {
